4ab33f98e554a845ce0c878074221b97b11d8574
Sothpanha682 <161024360+Sothpanha682@users.noreply.github.com>
2024-05-03 16:25:34 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+{
+    "/frontend/www": {
+        "url": "https://www.bczin.com/"
+    },
+    "/another-frontend/www": {
+        "url": "https://www.bczin.com/",
+        "ignoreSSLErrors": true
+    }
+}
